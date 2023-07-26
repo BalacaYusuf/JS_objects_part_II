486f97ca8e3b193f080e6db43794b44ba524d433
BalacaYusuf <xamoyev.royal1998@gmail.com>
2023-07-26 13:03:35 +0300

    commit

---
+++ 2-task.js
@@ -0,0 +1,71 @@
+
+
+// ---------------------------Write a program which can empty a given object-----------------------
+// const emptyObj={};
+
+// let person=
+// {
+//   name :'Ahmad',
+//  surnname :'Baghirov',
+//   age:20
+// }
+
+
+
+// Object.setPrototypeOf(emptyObj,person);
+
+// for (const key in person) {
+
+  
+  
+   
+   
+    // if (person.hasOwnProperty(key)) {
+
+    //    delete person[key];
+        
+    // }
+   
+// }
+
+// console.log(person);
+// console.log(emptyObj); 
+
+// --------------------------- Create bigJS like Math methods-----------------------
+ 
+
+
+
+// Number.prototype.add=function(val){
+//     return this+val
+// };
+// Number.prototype.Multiple=function(val){
+//     return this*val
+// };
+// Number.prototype.divide=function(val){
+//     return this/val
+// };
+// Number.prototype.Substract=function(val){
+//     return this-val
+// };
+
+// const result = (5).add(3).Multiple(2).divide(4).Substract(2);
+
+  
+// console.log(result);
+
+
+
+
+// ---------------Create an object and make it behave like an array which allows push and pop operations on items
+
+// let UserOfArray ={
+//     name : 'Yusuf',
+//     age : 31,
+// }
+
+     
+//     let sm=Array.prototype.MyPop=Object.keys(UserOfArray);
+
+//     console.log(sm.pop(name));
+    
